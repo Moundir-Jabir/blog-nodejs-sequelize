54858c381a5d9090d66fde84a56c1dccb6cf31ba
meryemeelhajouji <meryemelhajouji.99@gmail.com>
2022-09-29 14:22:03 +0200

    create new article

--- controllers/articleController.js
+++ controllers/articleController.js
@@ -1,21 +1,30 @@
 const { Article } = require('../models/articles')
 
+
 exports.getarticles = (req, res) => {
     Article.findAll().then(data => res.json({data}))
         .catch(err => res.status(400).json({err}))
       
-            const  Article = {
-               title: req.body.title,
-               image:req.body.image,
-               content: req.body.content
-            };
-            Article.createArticle(article).then(data =>{
-               res.send(data);
-            }).catch(err =>{
-              console.log("error");
-            })
-            }
-
-
-
-
+       
+// exports.createArticle = (req, res) => {
+//          if (!req.body.title) {
+//            res.status(400).send({
+//              message: "Content can not be empty!"
+//            });
+//            return;
+//          }// 
+//      const  article = {
+//                title: req.body.title,
+//                image:req.body.image,
+//                content: req.body.content
+//             };
+//     Article.createArticle(article)
+//      .then(data =>{
+//                res.send(data);
+//             }) .catch(err => {
+//                res.status(500).send({
+//                  message:
+//                    err.message || "Some error occurred while creating the Tutorial."
+//                });
+//              });
+//  }}
--- routes/article.js
+++ routes/article.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const router = express.Router()
 const { getarticles } = require('../controllers/articleController')
+const { createArticle } = require('../controllers/articleController')
 
 
 router.get('/', getarticles)
